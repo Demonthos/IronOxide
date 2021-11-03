73067900c0ea0d83cd737654025aa9f7c3ced798
Demonthos <66571940+Demonthos@users.noreply.github.com>
2021-11-03 10:43:39 -0500

    Add files via upload

--- lib/src/renderer.rs
+++ lib/src/renderer.rs
@@ -10,12 +10,12 @@ use specs::{Component, VecStorage};
 pub enum Renderer {
     CircleRenderer { radius: f32, color: Color },
     RectangeRenderer { size: Vector2, color: Color },
-    SpriteRenderer { img: Texture2D , size: Rectangle, tint: Color },
+    SpriteRenderer { img: Texture2D , scale: f32, tint: Color },
 }
 
 impl Renderer {
-    pub fn image(path: &str, size: Vector2, tint: Color, rl: &mut RaylibHandle, rlth: &RaylibThread) -> Renderer{
-        Renderer::SpriteRenderer{img: rl.load_texture_from_image(rlth, &Image::load_image(path).unwrap()).unwrap(), size: Rectangle{x: 0.0, y: 0.0, width: size.x, height: size.y}, tint: tint}
+    pub fn image(path: &str, scale: f32, tint: Color, rl: &mut RaylibHandle, rlth: &RaylibThread) -> Renderer{
+        Renderer::SpriteRenderer{img: rl.load_texture_from_image(rlth, &Image::load_image(path).unwrap()).unwrap(), scale, tint: tint}
     }
 
     pub fn render(&self, d: &mut impl raylib::core::drawing::RaylibDraw, position: &Position) {
@@ -26,8 +26,8 @@ impl Renderer {
             Renderer::RectangeRenderer { size, color } => {
                 d.draw_rectangle_v(position.0, size, color);
             },
-            Renderer::SpriteRenderer{img, size, tint} => {
-                 d.draw_texture_rec(img, size, position.0, tint)
+            Renderer::SpriteRenderer{img, scale, tint} => {
+                 d.draw_texture_ex(img, position.0, *scale, 0.0, tint)
             }
         }
     }
